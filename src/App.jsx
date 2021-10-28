73e3812d70deaed54a2f805cac82fd290dc5c3bf
Patrick Obermeier <pato@xxxlgroup.com>
2021-10-28 23:10:17 +0200

    renamed files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import "./normalize.css";
 import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
       <h1>Hello Litty!</h1>
       <h2>The shitty kitty</h2>
+      <Footer companyName="Buschbär &amp; Litty Inc" />
     </div>
   );
 }
